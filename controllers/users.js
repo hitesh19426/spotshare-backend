674de362f552aac096e96fe3a838b019f9da625f
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-03-15 10:30:06 +0530

    added mongoDB logic in signup and login user

--- controllers/users.js
+++ controllers/users.js
@@ -3,11 +3,18 @@ const { validationResult } = require("express-validator");
 const User = require("./../models/users")
 
 const HttpError = require("./../models/http-error");
-const { USERS, PLACES } = require("./../DUMMY_DATA");
 
-const getListOfUsers = (req, res, next) => {
+const getListOfUsers = async (req, res, next) => {
   console.log("will return a list of users");
-  res.json({ message: "returned users!!", USERS });
+
+  try{
+    const users = await User.find({}, 'name email imageUrl');
+    return res.json({message: "Users Found", users: users.map(user => user.toObject({getters: true}))});
+  }catch(err){
+    return next(new HttpError('Error finding users. Please try again'));
+  }
+
+  // res.json({ message: "returned users!!", USERS });
 };
 
 const SignUpUser = async (req, res, next) => {
@@ -19,11 +26,6 @@ const SignUpUser = async (req, res, next) => {
   console.log("want to signup user");
   const { name, email, password, places } = req.body;
 
-  // const hasUser = USERS.find((user) => user.email === email);
-  // if (hasUser) {
-  //   return next(new HttpError("User already exists", 401));
-  // }
-
   try{
     const existingUser = await User.findOne({email: email})
     if(existingUser){
@@ -33,12 +35,7 @@ const SignUpUser = async (req, res, next) => {
     return next(new HttpError("Signing up failed, try again later", 500));
   }
   
-  const newUser = new User({
-    name,
-    email,
-    password,
-    places
-  })
+  const newUser = new User({name, email, password, places })
 
   try{
     await newUser.save()
@@ -46,14 +43,10 @@ const SignUpUser = async (req, res, next) => {
   }catch(err){
     return next(new HttpError('Signup failed, try again', 500));
   }
-
-
-  // const newUser = { id: uuid.v4(), name, email, password };
-  // USERS.push(newUser);
   
 };
 
-const LoginUser = (req, res, next) => {
+const LoginUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(new HttpError(errors, 404));
@@ -62,12 +55,22 @@ const LoginUser = (req, res, next) => {
   console.log("want to login user");
   const { email, password } = req.body;
 
-  const user = USERS.find((user) => user.email === email);
+  let user;
+  try{
+    user = await User.findOne({email: email});
+  }catch(err){
+    return next(new HttpError('Error occured while fetching user. Try again.', 401))
+  }
+  
+  // const user = USERS.find((user) => user.email === email);
+  user = user.toObject({getters: true});
   if (!user || password !== user.password) {
     return next(new HttpError("Wrong credentials", 401));
   }
 
-  res.json({ message: "loged in user!!" });
+  // TODO: Exclude password while sending response
+  const requiredUser = ( ({name, email, imageUrl, places, id}) => ({name, email, imageUrl, places, id}) )(user);
+  res.json({ message: "loged in user!!", user: requiredUser});
 };
 
 exports.getListOfUsers = getListOfUsers;
