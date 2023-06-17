d47b43eaeb47b65978b72d295aa38ca8d2b0ef7d
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-06-17 12:52:18 +0530

    set cors headers

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ require('dotenv').config()
 
 const app = express()
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+    next()
+})
+
 app.use('/api/places', placeRouter)
 app.use('/api/users', userRouter)
 
--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ router.post(
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 8 }),
+    check("password").isLength({ min: 6 }),
   ],
   SignUpUser
 );
@@ -24,7 +24,7 @@ router.post(
   "/login",
   [
     check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 8 }),
+    check("password").isLength({ min: 6 }),
   ],
   LoginUser
 );
